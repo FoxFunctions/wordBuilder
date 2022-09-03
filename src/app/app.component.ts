a8e5986edf716b7e0fadf5f0a71c591610cf2d85
Austin Howell <austinhowell@Austins-MacBook-Pro.local>
2022-09-03 13:14:13 -0400

    resolved issue with css transitions not working correctly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
-import { icon } from './iconModel';
 import { image } from './icon';
 
 
@@ -11,43 +10,47 @@ import { image } from './icon';
 })
 
 export class AppComponent {
-  constructor(public imageHelper : image) {}
+  constructor(public imageHelper: image) { }
   title = 'wordBuilder';
-  usersAlphabet: string[] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n',
-  'o','p','q','r','s','t','u','v','w','x','y','z'];
-  myWord: string[] = [ 
+  usersAlphabet: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  myWord: string[] = [
   ];
-  userWord : string = "";
-  isCorrectSpelling : boolean = false;
+  userWord: string = "";
+  isCorrectSpelling: boolean = false;
 
-  spelledCorrectly() : boolean {
-    if (this.userWord == this.imageHelper.placeHolderHelperIcon.title){
+  spelledCorrectly(): boolean {
+    if (this.userWord == this.imageHelper.placeHolderHelperIcon.title) {
       this.isCorrectSpelling = true;
     }
     return this.isCorrectSpelling;
   }
 
-  resetEnglishAlphabet() : void {
-  this.usersAlphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n',
-  'o','p','q','r','s','t','u','v','w','x','y','z'];
-  }
-  clearWordList() : void {
-    this.myWord = [];
-  }
-  myWordToString() : string {
-  this.userWord = this.myWord.join('');
-  console.log("userWord =" + this.userWord);
-  return this.userWord;
+  resetEnglishAlphabet(): void {
+    this.usersAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+      'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   }
 
-  iconIterator() : void {
+  clearWordList(): void {
+    this.myWord = [];
+    this.isCorrectSpelling = false;
+  }
+
+  myWordToString(): string {
+    this.userWord = this.myWord.join('');
+    return this.userWord;
+  }
+
+  iconIterator(): void {
     this.imageHelper.iconIterator();
     this.clearWordList();
+    this.isCorrectSpelling = false;
   }
+
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else{
+    } else {
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
@@ -57,6 +60,5 @@ export class AppComponent {
     }
     this.resetEnglishAlphabet();
     this.myWordToString();
-    console.log(this.usersAlphabet);
   }
 }
